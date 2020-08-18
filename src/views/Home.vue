a918c7e224bba8c063da42e328182fedb8211402
Bojana Corilic <bojana.corilic@vivifyideas.com>
2020-08-18 09:05:33 +0200

    Movie List

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,14 @@
 <template>
-  <h1 align="center"> Imdb for Movies </h1><br>
+  <h1 align="center">Imdb for Movies</h1>
+  <br />
   <div class="row">
-  <div class="column" v-for="movie in movies" :key="movie">
-    <div class="card">
-      <h3>{{movie.title}}</h3>
-      <p>{{movie.description}}</p>
-      <img class="image" v-bind:src= "movie.image_url">
+    <div class="column" v-for="movie in movies" :key="movie">
+      <div class="card" style="cursor:pointer;" @click="getMovie(movie)">
+        <h3>{{movie.title}}</h3>
+        <p>{{trucateText(movie.description)}}</p>
+        <img class="image" v-bind:src="movie.image_url" />
+      </div>
     </div>
-  </div>
-  
   </div>
 </template>
 
@@ -16,14 +16,14 @@
 import { mapActions } from "vuex";
 
 export default {
-  name: 'Home',
-  mounted () {
+  name: "Home",
+  mounted() {
     this.getAllMovies();
   },
-  data () {
+  data() {
     return {
-      movies: null
-    }
+      movies: null,
+    };
   },
   methods: {
     ...mapActions("movies", ["getMovies"]),
@@ -32,46 +32,63 @@ export default {
       this.movies = data;
       console.log(data);
     },
+    trucateText(value) {
+      const length = 230;
+      return value.length <= length
+        ? value
+        : value.substring(0, length) + "...";
+    },
+    getMovie(oneMovie){
+      const path = '/movie/' + oneMovie.title;
+      this.$router.push({path: path, 
+      props: {
+        title: oneMovie.title,
+        description: oneMovie.description,
+        image_url: oneMovie.image_url,
+      }});
+    }
   },
-}
+};
 </script>
 
 <style >
+.column {
+  float: left;
+  width: 25%;
+  padding: 0 10px;
+}
+
+/* Remove extra left and right margins, due to padding */
+.row {
+  margin: 0 -5px;
+}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+/* Responsive columns */
+@media screen and (max-width: 600px) {
   .column {
-    float: left;
-    width: 25%;
-    padding: 0 10px;
+    width: 100%;
+    display: block;
+    margin-bottom: 20px;
   }
+}
 
-  /* Remove extra left and right margins, due to padding */
-  .row {margin: 0 -5px;}
+/* Style the counter cards */
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  padding: 16px;
+  text-align: center;
+  background-color: #f1f1f1;
+}
 
-  /* Clear floats after the columns */
-  .row:after {
-    content: "";
-    display: table;
-    clear: both;
-  }
-
-  /* Responsive columns */
-  @media screen and (max-width: 600px) {
-    .column {
-      width: 100%;
-      display: block;
-      margin-bottom: 20px;
-    }
-  }
-
-  /* Style the counter cards */
-  .card {
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-    padding: 16px;
-    text-align: center;
-    background-color: #f1f1f1;
-  }
-
-  .image{
-    width: 250px;
-    height: 250px;
-  }
+.image {
+  width: 250px;
+  height: 250px;
+}
 </style>>
---
+++ src/views/Movie.vue
@@ -0,0 +1,35 @@
+<template>
+
+  <section class="movie">
+    <h1>{{title}}</h1>
+  </section>
+
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'Movie',
+    props: ['title', 'description', 'image_url'],
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style>
+
+</style>
