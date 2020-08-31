be3a7685137df24a5dffdbf7103c0000761f6f37
Bojana Corilic <bojana.corilic@vivifyideas.com>
2020-08-31 08:09:04 +0200

    Create Movie OMDB

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
       <router-link to="/register" v-show="login">Register</router-link>
       <router-link to="/watchlist" v-show="logout">Watch list</router-link>|
       <router-link to="/createMovie" v-show="logout">Create Movie</router-link>|
+      <router-link to="/createMovieOMDB" v-show="logout">Create Movie OMDB</router-link>|
       <router-link to="/logout" v-show="logout">Logout</router-link>
       <router-link to="/login" v-show="login">Login</router-link>    
     </div>
